6553c7d54376e53655b486deecb49fb9c46ba420
HomS <obr_dev@protonmail.com>
2024-08-31 10:52:38 -0600

    Implemented TransactionService

---
+++ src/main/java/com/homsdev/account_manager_cli/service/TransactionService.java
@@ -0,0 +1,51 @@
+package com.homsdev.account_manager_cli.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.homsdev.account_manager_cli.exceptions.ResourceNotCreatedException;
+import com.homsdev.account_manager_cli.exceptions.ResourceNotFoundException;
+import com.homsdev.account_manager_cli.exceptions.ResourceNotUpdatedException;
+import com.homsdev.account_manager_cli.model.Transaction;
+import com.homsdev.account_manager_cli.repository.TransactionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public Transaction saveTransaction(Transaction transaction) {
+        return transactionRepository.saveTransaction(transaction)
+                .orElseThrow(() -> new ResourceNotCreatedException("Transaction was not created"));
+    }
+
+    public List<Transaction> findAllByAccountId(String accountId) {
+        return transactionRepository.findAllByAccountId(accountId);
+    }
+
+    public Transaction findById(String transactionId) {
+        return transactionRepository.findById(transactionId)
+                .orElseThrow(() -> new ResourceNotFoundException("Transaction not found"));
+    }
+
+    public Transaction updateTransaction(Transaction transaction) {
+        return transactionRepository.updateTransaction(transaction)
+                .orElseThrow(() -> new ResourceNotUpdatedException("Transaction was not updated"));
+    }
+
+    public Integer deleteTransaction(String transactionId) {
+        Integer result = transactionRepository.deleteTransaction(transactionId);
+
+        if (result == 0) {
+            throw new ResourceNotFoundException("Transaction not deleted");
+        }
+
+        return transactionRepository.deleteTransaction(transactionId);
+    }
+
+}
